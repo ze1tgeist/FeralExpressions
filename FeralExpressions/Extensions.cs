dd811562ce26805233bec8d710dd29be0b56a599
ruben.morton <ruben.morton@opc.gov.au>
2017-07-25 15:47:31 +1000

    moved inline wrapper to a seperate file

--- FeralExpressions/Extensions.cs
+++ FeralExpressions/Extensions.cs
@@ -22,54 +22,5 @@ namespace FeralExpressions
             return new InlineWrapper<T>(queryable);
         }
 
-        private class InlineWrapper<T> : IQueryable<T>
-        {
-            public InlineWrapper(IQueryable<T> inner)
-            {
-                this.inner = inner;
-            }
-
-            public Expression Expression => inner.Expression.Inline();
-
-            public Type ElementType => typeof(T);
-
-            public IQueryProvider Provider => throw new NotImplementedException();
-
-            public IEnumerator<T> GetEnumerator()
-            {
-                throw new NotImplementedException();
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                throw new NotImplementedException();
-            }
-
-            private class QueryProvider : IQueryProvider
-            {
-                public IQueryable CreateQuery(Expression expression)
-                {
-                    throw new NotImplementedException();
-                }
-
-                public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
-                {
-                    throw new NotImplementedException();
-                }
-
-                public object Execute(Expression expression)
-                {
-                    throw new NotImplementedException();
-                }
-
-                public TResult Execute<TResult>(Expression expression)
-                {
-                    throw new NotImplementedException();
-                }
-            }
-
-            private IQueryable<T> inner;
-
-        }
     }
 }
