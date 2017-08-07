71936f3404ea3fee7da1d0e0d369c5c673c6332a
Ruben Morton <ruben.morton@gmail.com>
2017-08-07 18:15:22 +1000

    added nuspec versions

--- build_nuget.cmd
+++ build_nuget.cmd
@@ -1,2 +1,2 @@
 "C:\Program Files (x86)\NuGet\nuget.exe" pack FeralExpressions.Generator\FeralExpressions.Generator.nuspec -OutputDirectory outputPackages
-"C:\Program Files (x86)\NuGet\nuget.exe" pack FeralExpressions\FeralExpressions.csproj -OutputDirectory outputPackages
+"C:\Program Files (x86)\NuGet\nuget.exe" pack FeralExpressions\FeralExpressions.nuspec -OutputDirectory outputPackages
