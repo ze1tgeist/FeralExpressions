dc31eae4a4f6c21e648a08039342eced8bd011ad
ruben.morton <ruben.morton@AWS125VDEDEV09>
2019-03-15 09:30:50 +1100

    added classes to help test the override/new/virtual bug

---
+++ FeralExpressionsCore.Generator.Tests/BaseClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeralExpressionsCore.Generator.Tests
+{
+    public partial class BaseClass
+    {
+        public virtual int VirtualMethodInBaseClass() => 0;
+    }
+}
---
+++ FeralExpressionsCore.Generator.Tests/SubClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeralExpressionsCore.Generator.Tests
+{
+    public partial class SubClass : BaseClass
+    {
+        public override int VirtualMethodInBaseClass() => 1;
+    }
+}
